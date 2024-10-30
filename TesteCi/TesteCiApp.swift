0c5e8a127b614ed51907c47d5e090f98af3dafd9
Junior Fernandes <42782419+juniorfernandesdev@users.noreply.github.com>
2024-10-29 22:13:42 -0400

    ininital commit

---
+++ TesteCi/TesteCiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteCiApp.swift
+//  TesteCi
+//
+//  Created by Junior Fernandes on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TesteCiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
